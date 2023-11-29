0eb9aef0e8b308dae0b7a807098fb9db5ad78216
MATSIM <s.nasirahmed34@gmail.com>
2023-11-29 22:01:32 +0900

    80% Validation Odakyu3. Able to move forward with other Validation tasks and SAV simulations.
    Used MATSimTransitNetworkProcessor.updatePtLinkSpeed to increase pt link speed from 5.55 m/s to 20 m/s or 20 to 70 km/h
    Allowed bike access mode without restraining by bike_avail.
    
    Increased RailRaptor routeSelector Transfer parameter to 3000.0 to discourage useless distances. Increased transferPenaltyBaseCost to 200 and transferPenaltyCostPerTravelTimeHour to 200.
    
    Used modeAvailability Car and Default, though it may have been upside down.
    
    ACHEIVED A RELATIVELY BETTER PT TRAVEL TIME VALIDATION. STILL A GREAT VARIATION IN TERMS OF SPEED AND VARIATION BUT THE TREND IS GENERALLY THE SAME.
    
    Moving on from pt Validation to car validation. Too fast, increase of traffic and decrease flow capacity. switch modeAvailability:Car
    
    Reduced flowCapacityFactor and storageCapacityFactor to 0.1, around 10% of normal.

---
+++ contribs/SyntheticModel/src/main/java/TransitCreator/Cleaners/MATSimTransitNetworkProcessor.java
@@ -0,0 +1,77 @@
+package TransitCreator.Cleaners;
+
+import org.matsim.api.core.v01.Scenario;
+import org.matsim.api.core.v01.network.Network;
+import org.matsim.api.core.v01.network.Link;
+import org.matsim.core.config.ConfigUtils;
+import org.matsim.core.network.io.MatsimNetworkReader;
+import org.matsim.core.network.io.NetworkWriter;
+import org.matsim.core.scenario.ScenarioUtils;
+
+
+public class MATSimTransitNetworkProcessor {
+
+	public void updatePtLinkSpeed(String networkFilePath, String outputPath) throws Exception {
+		// Load the network
+		Scenario scenario = ScenarioUtils.createScenario(ConfigUtils.createConfig());
+		new MatsimNetworkReader(scenario.getNetwork()).readFile(networkFilePath);
+
+		Network network = scenario.getNetwork();
+		double newSpeed = 20.0 ; // 72 km/h to  20 m/s
+
+		// Iterate through all links in the network
+		for (Link link : network.getLinks().values()) {
+			// Check if 'pt' is in the allowed modes
+			if (link.getAllowedModes().contains("pt")) {
+				// Set the new speed
+				link.setFreespeed(newSpeed);
+			}
+		}
+
+		// Write the modified network back to the file
+		new NetworkWriter(network).write(outputPath);
+	}
+
+	public void updateCarLinkSpeed(String networkFilePath, String outputPath) throws Exception {
+		// Load the network
+		Scenario scenario = ScenarioUtils.createScenario(ConfigUtils.createConfig());
+		new MatsimNetworkReader(scenario.getNetwork()).readFile(networkFilePath);
+
+		Network network = scenario.getNetwork();
+
+		// Iterate through all links in the network
+		for (Link link : network.getLinks().values()) {
+			// Check if 'car' is in the allowed modes
+			if (link.getAllowedModes().contains("car")) {
+				double currentSpeed = link.getFreespeed();
+				double newSpeed = determineNewSpeed(currentSpeed);
+
+				// Set the new speed
+				link.setFreespeed(newSpeed);
+			}
+		}
+
+		// Write the modified network back to the file
+		new NetworkWriter(network).write(outputPath);
+	}
+
+	private double determineNewSpeed(double currentSpeed) {
+		if (currentSpeed == 4.166666666666667) return 8.333333333333334;
+		if (currentSpeed == 8.333333333333334) return 11.11111111111111;
+		if (currentSpeed == 1.11111111111111) return 12.5;
+		if (currentSpeed == 12.5) return 13.88888888888889;
+		if (currentSpeed == 13.88888888888889) return 20;
+		if (currentSpeed == 22.22222222222222) return 25.0;
+
+		return currentSpeed; // Default case, no change
+	}
+
+	public static void main(String[] args) throws Exception {
+		String inputFilePath = "examples/scenarios/Odakyu3/network2.xml"; // Replace with your input file path
+		String outputFilePath = "examples/scenarios/Odakyu3/network2.xml"; // Replace with your output file path
+
+		new MATSimTransitNetworkProcessor().updatePtLinkSpeed(inputFilePath, outputFilePath);
+
+
+	}
+}
