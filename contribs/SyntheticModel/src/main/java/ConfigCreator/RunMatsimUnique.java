1024b9f220260e4b3ca0239f402e2643dc92a1c6
synas34 <s.nasirahmed34@gmail.com>
2023-12-28 19:02:31 +0900

    Updating some work to server. unsure of what work was doen before holidays. Hoping to start again by running an urbanisation level segmented model

--- contribs/SyntheticModel/src/main/java/ConfigCreator/RunMatsimUnique.java
+++ contribs/SyntheticModel/src/main/java/ConfigCreator/RunMatsimUnique.java
@@ -2,8 +2,6 @@ package ConfigCreator;
 
 import MyDMC.*;
 import MyDMC.Trial.SAVasRideDMCExtension;
-import ch.sbb.matsim.config.SwissRailRaptorConfigGroup;
-import ch.sbb.matsim.routing.pt.raptor.RaptorParameters;
 import org.matsim.api.core.v01.Scenario;
 import org.matsim.contrib.drt.extension.preplanned.run.PreplannedDrtModeModule;
 import org.matsim.contrib.drt.run.*;
@@ -29,25 +27,21 @@ import org.matsim.pt.transitSchedule.api.TransitSchedule;
 import java.awt.Desktop;
 import java.io.File;
 import java.io.IOException;
-import java.util.TreeMap;
 
 
 public class RunMatsimUnique {
 
 
-	public static void main(String[] args) throws IOException {
+	public static void RunSAVSimulation (String configpath, String outputpath, AbstractDiscreteModeChoiceExtension DMCExtension) throws IOException {
+
 
 		Config config;
-		if ( args==null || args.length==0 || args[0]==null ){
-			config = ConfigUtils.loadConfig( "examples/scenarios/Odakyu4/configSAV2.xml", new MultiModeDrtConfigGroup(),
-				new DvrpConfigGroup(), new OTFVisConfigGroup(),new DiscreteModeChoiceConfigGroup(), new SwissRailRaptorConfigGroup());
-		} else {
-			config = ConfigUtils.loadConfig( args );
-		}
+			config = ConfigUtils.loadConfig( configpath, new MultiModeDrtConfigGroup(),
+				new DvrpConfigGroup(), new OTFVisConfigGroup(),new DiscreteModeChoiceConfigGroup());
 
 
 		config.controler().setOverwriteFileSetting( OutputDirectoryHierarchy.OverwriteFileSetting.deleteDirectoryIfExists );
-		config.controler().setOutputDirectory("examples/scenarios/Odakyu4/outputDec14(200)NewUtil-1seatSAV");
+		config.controler().setOutputDirectory(outputpath);
 		// possibly modify config here
 		config.qsim().setSimStarttimeInterpretation(QSimConfigGroup.StarttimeInterpretation.onlyUseStarttime);
 		config.qsim().setSimEndtimeInterpretation((QSimConfigGroup.EndtimeInterpretation.onlyUseEndtime));
@@ -56,13 +50,75 @@ public class RunMatsimUnique {
 
 		// Add Discrete Choice Module
 		controller.addOverridingModule(new DiscreteModeChoiceModule());
-		controller.addOverridingModule(new NasirSAVDMCExtension());
+		controller.addOverridingModule(DMCExtension);
 		DiscreteModeChoiceConfigurator.configureAsModeChoiceInTheLoop(config);
 
 
 		// Run the simulation
 		controller.run();
+
 		Desktop.getDesktop().open(new File(config.controler().getOutputDirectory() + "/modestats.txt"));
+
+
+
+	}
+
+	public static void main(String[] args) throws IOException {
+
+		Config config;
+		if ( args==null || args.length==0 || args[0]==null ){
+			config = ConfigUtils.loadConfig( "examples/scenarios/Odakyu4/configSAV2.xml", new MultiModeDrtConfigGroup(),
+				new DvrpConfigGroup(), new OTFVisConfigGroup(),new DiscreteModeChoiceConfigGroup());
+		} else {
+			config = ConfigUtils.loadConfig( args );
+		}
+
+		config.controler().setOverwriteFileSetting( OutputDirectoryHierarchy.OverwriteFileSetting.deleteDirectoryIfExists );
+		config.controler().setOutputDirectory("examples/scenarios/Odakyu4/output(200)Dec5-HighFare");
+		// possibly modify config here
+		config.qsim().setSimStarttimeInterpretation(QSimConfigGroup.StarttimeInterpretation.onlyUseStarttime);
+		config.qsim().setSimEndtimeInterpretation((QSimConfigGroup.EndtimeInterpretation.onlyUseEndtime));
+
+		Controler controller = DrtControlerCreator.createControler(config, false);
+
+		// Add Discrete Choice Module
+		controller.addOverridingModule(new DiscreteModeChoiceModule());
+		controller.addOverridingModule(new NDMCExtensionHighFare());
+		DiscreteModeChoiceConfigurator.configureAsModeChoiceInTheLoop(config);
+
+		// Run the simulation
+		controller.run();
+		Desktop.getDesktop().open(new File(config.controler().getOutputDirectory() + "/modestats.txt"));
+
+
+
+
+
+		if ( args==null || args.length==0 || args[0]==null ){
+			config = ConfigUtils.loadConfig( "examples/scenarios/Odakyu4/configSAV2.xml", new MultiModeDrtConfigGroup(),
+				new DvrpConfigGroup(), new OTFVisConfigGroup(),new DiscreteModeChoiceConfigGroup());
+		} else {
+			config = ConfigUtils.loadConfig( args );
+		}
+
+		config.controler().setOverwriteFileSetting( OutputDirectoryHierarchy.OverwriteFileSetting.deleteDirectoryIfExists );
+		config.controler().setOutputDirectory("examples/scenarios/Odakyu4/output(200)Dec5-LowFare");
+		// possibly modify config here
+		config.qsim().setSimStarttimeInterpretation(QSimConfigGroup.StarttimeInterpretation.onlyUseStarttime);
+		config.qsim().setSimEndtimeInterpretation((QSimConfigGroup.EndtimeInterpretation.onlyUseEndtime));
+
+		controller = DrtControlerCreator.createControler(config, false);
+
+		// Add Discrete Choice Module
+		controller.addOverridingModule(new DiscreteModeChoiceModule());
+		controller.addOverridingModule(new NDMCExtensionLowFare());
+		DiscreteModeChoiceConfigurator.configureAsModeChoiceInTheLoop(config);
+
+		// Run the simulation
+		controller.run();
+
+		Desktop.getDesktop().open(new File(config.controler().getOutputDirectory() + "/modestats.txt"));
+
 
 	}
 }
--- matsim/src/main/java/ch/sbb/matsim/routing/pt/raptor/DefaultRaptorIntermodalAccessEgress.java
+++ matsim/src/main/java/ch/sbb/matsim/routing/pt/raptor/DefaultRaptorIntermodalAccessEgress.java
@@ -32,13 +32,8 @@ public class DefaultRaptorIntermodalAccessEgress implements RaptorIntermodalAcce
                 OptionalTime travelTime = ((Leg) pe).getTravelTime();
                 if (travelTime.isDefined()) {
                     tTime += travelTime.seconds();
-					disutility += travelTime.seconds() * -params.getMarginalUtilityOfTravelTime_utl_s(mode);
-//					if (mode.equals("transit_walk")) {
-//						disutility += travelTime.seconds() * (-params.getMarginalUtilityOfTravelTime_utl_s(mode) * 40.5);
-//					} else {
-//						disutility += travelTime.seconds() * -params.getMarginalUtilityOfTravelTime_utl_s(mode);
-//					}
-				}
+                    disutility += travelTime.seconds() * -params.getMarginalUtilityOfTravelTime_utl_s(mode);
+                }
             }
             else if (pe instanceof Activity) {
             	if (((Activity) pe).getMaximumDuration().isDefined()) {
