73d3528dc3b60b20a7302d414a646f4258de2796
MATSIM <s.nasirahmed34@gmail.com>
2024-05-07 10:16:05 +0900

    May 4th following from april 22nd and 18th,
    Uploading results of Geofence simulation.
    
    -literature review of possible Value of Time estimates
    -run sensitivity analysis through multiple parameter scenarios
    
    Then this will lead into:
    - New Geofence/Area using QGIS
    - Revenue Tracker for each fleet
    - Analysis by extending previous analyses above.

--- contribs/SyntheticModel/src/main/java/ConfigCreator/RunMatsimUnique.java
+++ contribs/SyntheticModel/src/main/java/ConfigCreator/RunMatsimUnique.java
@@ -114,8 +114,8 @@ public class RunMatsimUnique {
 
 //			new SimulationConfig("examples/scenarios/Odakyu6/configGEOSIMPLE.xml", new UrbanIndexDMCExtensionSIMPLE_15()),
 //			new SimulationConfig("examples/scenarios/Odakyu6/configGEOSIMPLE.xml", new UrbanIndexDMCExtensionSIMPLE_x2())
-			new SimulationConfig("examples/scenarios/Odakyu6/configSAVSIMPLE.xml", new UrbanIndexDMCExtensionSIMPLE_COST()),
-			new SimulationConfig("examples/scenarios/Odakyu6/configGEOSIMPLE.xml", new UrbanIndexDMCExtensionSIMPLE_COST())
+//			new SimulationConfig("examples/scenarios/Odakyu6/configSAVSIMPLE.xml", new UrbanIndexDMCExtensionSIMPLE_COST()),
+			new SimulationConfig("examples/scenarios/Odakyu6/configGEOSIMPLE.xml", new UrbanIndexDMCExtensionSIMPLE_15())
 
 //			new SimulationConfig("examples/scenarios/Odakyu6/configGEOSIMPLE.xml", new UrbanIndexDMCExtensionSIMPLE())
 
